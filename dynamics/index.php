af902f647ee4bd8e2ef8fc4dc07387207985463e
Francisco-Galindo <paqui10718@gmail.com>
2021-05-07 17:47:29 -0500

    FEAT: Inicio básico de sesión

--- dynamics/index.php
+++ dynamics/index.php
@@ -1,3 +1,14 @@
 <?php
-    echo "LOL";
+    session_start();
+
+    if (!isset($_SESSION["nombre"])) {
+        echo "sesión iniciada";
+        
+    }
+    
+    echo "Hola";
 ?>
+
+<form action="cerrar.php" method="POST">
+    <input type="submit" value="Cerrar sesión">
+</form>
