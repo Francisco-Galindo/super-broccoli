f8ce8b51215399aec60700c66aec1d7b81f65958
Francisco-Galindo <paqui10718@gmail.com>
2021-05-08 21:44:08 -0500

    FEAT: Query para ver las descargas

--- dynamics/descargas.php
+++ dynamics/descargas.php
@@ -2,34 +2,26 @@
 $c = mysqli_connect("localhost", "root", "");
 $db = mysqli_select_db($c, "biblioteca");
 
-$consulta = "SELECT 
-l.id_libro, 
-l.libro, 
-h.fecha, 
-FROM
-libros l
-LEFT JOIN historial_descargas h USING(id_libro)
-WHERE id_libro IS NOT NULL";
-
-$consulta2 = "SELECT 
-u.id_usuario, 
-u.nombre
-u.primer apellido
-u.segundo apellido, 
-FROM
-usuario
-LEFT JOIN historial_descargas h USING(id_usuario)
-WHERE id_usuario IS NOT NULL";
-
-echo'
-<table>
-    <tr>
-        <td>$consulta2</td>
-        <td>$consulta</td>
-    </tr>
-</table>';
+$consulta = "SELECT fecha, titulo, autor, nombre
+FROM historial_descargas t1
+INNER JOIN libros t2 ON t1.id_libro = t2.id_libro
+INNER JOIN usuario t3 ON t1.id_usuario = t3.num_cuenta_rfc
+ORDER BY fecha DESC;";
 
 $r = mysqli_query($c, $consulta);
+
+while($row=mysqli_fetch_array($r))
+{
+    echo "<br>";
+	echo $row["fecha"];
+    echo "<br>";
+	echo $row["nombre"];
+    echo "<br>";
+	echo $row["titulo"];
+    echo "<br>";
+	echo $row["autor"];
+    echo "<br>";
+}
 
 
 ?>
