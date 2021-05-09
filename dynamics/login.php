f01be47aa8713e50a7c7d682256b8a8b11cdbdac
Francisco-Galindo <paqui10718@gmail.com>
2021-05-08 19:04:09 -0500

    FEAT: Añadiendo validación a los archivos php de iniciar sesión y crear usuario

--- dynamics/login.php
+++ dynamics/login.php
@@ -1,7 +1,12 @@
 <?php
-$contra = $_POST["contra"];
 
-$email = $_POST["email"];
+if (isset($_POST["email"])) {
+	$contra = $_POST["contra"];
+	$email = $_POST["email"];
+}
+else {
+	header("location: ../templates/login.html");
+}
 
 $c = mysqli_connect("localhost", "root", "");
 $db = mysqli_select_db($c, "biblioteca");
@@ -11,11 +16,14 @@ $consulta = "SELECT nombre FROM usuario WHERE correo='$email' AND contraseña='$
 
 $r = mysqli_query($c, $consulta);
 
+$contadorCoincidencias = 0;
 while($row=mysqli_fetch_array($r))
 {
 	echo($row["nombre"]);
+	$contadorCoincidencias ++;
 }
-
 mysqli_close($c);
+
+$contadorCoincidencias === 1 ? header("location: ./index.php") : header("location: ../templates/login.html");
 ?>
 
--- dynamics/register.php
+++ dynamics/register.php
@@ -1,38 +1,38 @@
 <?php
-$id = $_POST["num_cuenta"];
-$nombre = $_POST["nombre"];
-$prim_ape = $_POST["primer_apellido"];
-$seg_ape = $_POST["segundo_apellido"];
-$contra = $_POST["contra"];
-$email = $_POST["email"];
-$tipo = $_POST["tipo"];
+
+if (isset($_POST["num_cuenta"])) {
+	$id = $_POST["num_cuenta"];
+	$nombre = $_POST["nombre"];
+	$prim_ape = $_POST["primer_apellido"];
+	$seg_ape = $_POST["segundo_apellido"];
+	$contra = $_POST["contra"];
+	$email = $_POST["email"];
+	$tipo = $_POST["tipo"];
+}
+else {
+	header("location: ../templates/register.html");
+}
 
 $c = mysqli_connect("localhost", "root", "");
 $db = mysqli_select_db($c, "biblioteca");
 
 $consulta = "INSERT INTO usuario (num_cuenta_rfc, nombre, primer_apellido, segundo_apellido, contraseña, correo) VALUES ('$id', '$nombre', '$prim_ape', '$seg_ape', '$contra', '$email')";
-
-
 $r = mysqli_query($c, $consulta);
 
-var_dump($r);
-echo "<br>";
-
-$consulta = "CREATE USER '$nombre'@'localhost' IDENTIFIED BY '$contra'";
-$r = mysqli_query($c, $consulta);
-var_dump($r);
-echo "<br>";
-
-$id_tipo = "id_" . $tipo;
-$consulta = "INSERT INTO $tipo VALUES ('$id_tipo', '$id')";
-$r = mysqli_query($c, $consulta);
-var_dump($r);
-echo "<br>";
-
-$consulta = "GRANT ALL PRIVILEGES ON biblioteca.* TO  '$nombre'@'localhost'";
-$r = mysqli_query($c, $consulta);
-var_dump($r);
-echo "<br>";
-
+if ($r) {
+	$consulta = "CREATE USER '$nombre'@'localhost' IDENTIFIED BY '$contra'";
+	$r = mysqli_query($c, $consulta);	
+}
+if ($r) {
+	$id_tipo = "id_" . $tipo;
+	$consulta = "INSERT INTO $tipo VALUES ('$id_tipo', '$id')";
+	$r = mysqli_query($c, $consulta);
+}
+if ($r) {
+	$consulta = "GRANT ALL PRIVILEGES ON biblioteca.* TO  '$nombre'@'localhost'";
+	$r = mysqli_query($c, $consulta);
+}
 mysqli_close($c);
+
+$r ? header("location: ./index.php"); : header("location: ../templates/register.html");
 ?>
